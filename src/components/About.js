61f5058bd690c46e5a5f39b5ac81d5108f22d1de
Peace Kuria <ephraimekuria@gmail.com>
2025-10-22 15:49:00 +0300

    tests

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
+// src/components/About.js
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data.js";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about" data-testid="about">
+      <h2>About Me</h2>
+      <p>I enjoy solving real-world problems with code and continuous learning.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
